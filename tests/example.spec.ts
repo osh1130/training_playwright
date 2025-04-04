06ba8c4d721728636d805f6ba5a35d77f70a976e
osh1130 <vivizhao401@gmail.com>
2025-04-04 15:37:57 +1300

    testcase

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -16,3 +16,18 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test.only('check java page', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+  await page.getByRole('link',{name:'Get start'}).click();
+  await page.getByRole('button',{name:'Node.js'}).hover();
+  await page.getByText('Java',{exact:true}).click();
+
+  await expect(page).toHaveURL('https://playwright.dev/java/docs/intro');
+  await expect(page.getByText('Installing Playwright',{exact:true})).not.toBeVisible();
+  
+  const javadescription="Playwright was created specifically to accommodate the needs of end-to-end testing. Playwright supports all modern rendering engines including Chromium, WebKit, and Firefox. Test on Windows, Linux, and macOS, locally or on CI, headless or headed with native mobile emulation."
+  await expect(page.getByText(javadescription,{exact:true})).toBeVisible();
+});
+
+//npx playwright test tests/ --project=chromium
--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 
+
 test('test', async ({ page }) => {
   // Recording...
 });
